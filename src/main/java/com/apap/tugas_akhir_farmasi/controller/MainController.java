827d95b7382a80383cdedca6ed52bfcf486756b2
Rafif Shaputra <rafifshaputra25@gmail.com>
2018-11-23 20:17:07 +0700

    tambah login dan logout

--- src/main/java/com/apap/tugas_akhir_farmasi/controller/MainController.java
+++ src/main/java/com/apap/tugas_akhir_farmasi/controller/MainController.java
@@ -7,8 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	@RequestMapping("/")
-
-	private String home() {
+	public String home() {
 		return "home";
+	}
+	
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
 	}
 }
--- src/main/java/com/apap/tugas_akhir_farmasi/model/UserRoleModel.java
+++ src/main/java/com/apap/tugas_akhir_farmasi/model/UserRoleModel.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 
 @Entity
 @Table(name = "user")
-public class UserModel implements Serializable {
+public class UserRoleModel implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -26,6 +26,38 @@ public class UserModel implements Serializable {
     @Size(max = 255)
     @Column(name = "role" ,nullable = false)
     private String role;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 
 
 
