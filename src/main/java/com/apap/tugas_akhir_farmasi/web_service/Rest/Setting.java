6f7c46654a5de4fe42f34b5869c5f5f3f46d63bb
adrika-novrialdi <adrika.novrialdi@gmail.com>
2018-12-10 18:58:34 +0700

    Merge conflict

--- src/main/java/com/apap/tugas_akhir_farmasi/web_service/Rest/Setting.java
+++ src/main/java/com/apap/tugas_akhir_farmasi/web_service/Rest/Setting.java
@@ -5,7 +5,7 @@ public class Setting {
     final public static String getStafUrl = "http://si-appointment.herokuapp.com/api/getStaff/{id}";
     final public static String siRawatJalanAddMedicalSupplyUrl = "unknown";
     final public static String addBilling = "http://si-appointment.herokuapp.com/api/3/addBilling";
-    final public static String getLabKebutuhan = "http://si-laboratorium.herokuapp.com/lab/kebutuhan/perencanaan";
+    final public static String  getLabKebutuhan = "http://si-laboratorium.herokuapp.com/lab/kebutuhan/perencanaan";
     final public static String getLabKebutuhan2 = "https://ef19881e-faaf-4ff1-9038-3c8dad784235.mock.pstmn.io/lab/kebutuhan/perencanaan";
 
     public static String urlGenerator(String id , String text){
